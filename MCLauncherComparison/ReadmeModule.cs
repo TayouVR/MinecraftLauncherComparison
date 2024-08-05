cbd0f62d4f3796d0b89b8831a5962d06374760bf
Tayou <git@tayou.org>
2024-08-05 22:18:27 +0200

    update csproject name

---
+++ MCLauncherComparison/ReadmeModule.cs
@@ -0,0 +1,7 @@
+﻿public class ReadmeModule : Module
+{
+    /*protected override async Task<IEnumerable<IDocument>> ExecuteContextAsync(IExecutionContext context) {
+        string readmeText = await File.ReadAllTextAsync("../../readme.md");
+        return context.CreateDocument(await context.GetContentProviderAsync(readmeText)).Yield();
+    }*/
+}
