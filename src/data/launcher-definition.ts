56c4e9a2c395bcf012e0f9fe6ebafcf9880bdfc0
Tayou <git@tayou.org>
2025-03-10 22:25:55 +0100

    WIP Astro
    the framework is mostly in place, data is incomplete and messy, some fields don't have a display at all yet, some only have a very basic one

---
+++ src/data/launcher-definition.ts
@@ -0,0 +1,101 @@
+type LauncherConfiguration = {
+    license: string;
+    serverInstances: boolean;
+    lookAndFeel: string;
+};
+
+type FeatureModpack = {
+    name: string;
+    method: string;
+    updating: boolean;
+};
+
+type MinecraftResource = {
+    downloading: boolean;
+    updating?: boolean;
+    providers: string[];
+};
+
+type MinecraftResources = {
+    mods: MinecraftResource;
+    resourcepacks: MinecraftResource;
+    shaderpacks: MinecraftResource;
+    worlds: MinecraftResource;
+    datapacks: MinecraftResource;
+    plugins: MinecraftResource;
+};
+
+type JavaFeature = {
+    download: boolean;
+    versionManagement: boolean;
+    usingSystemInstall: boolean;
+};
+
+type Sandboxing = {
+    status: string;
+};
+
+type Overlay = {
+    status: string;
+}
+
+type Advertisement = {
+    status: string;
+    note: string;
+}
+
+type Features = {
+    modpacks: FeatureModpack[];
+    modloaders: string[];
+    minecraftResources: MinecraftResources;
+    import: string[];
+    export: string[];
+    java: JavaFeature;
+    sandboxing: Sandboxing;
+    overlay: Overlay;
+    advertisements: Advertisement;
+};
+
+type Artifact = {
+    arch: string[];
+    os: string;
+    link: string;
+};
+
+type Package = {
+    name: string;
+    official: boolean;
+};
+
+type Technology = {
+    languages: string[];
+    frameworks: string[];
+};
+
+type Properties = {
+    artifacts: Artifact[];
+    packages: Package[];
+    technology: Technology;
+};
+
+type SpecialFeature = {
+    supported: boolean;
+    method: string;
+};
+
+type SpecialFeatures = {
+    CurseForgeBlockedDownloads: SpecialFeature;
+};
+
+type LauncherDetails = {
+    identifier: string;
+    name: string;
+    icon: string;
+    downloadLink: string;
+    repoLink: string;
+    homepage: string;
+    configuration: LauncherConfiguration;
+    features: Features;
+    properties: Properties;
+    specialFeatures: SpecialFeatures;
+};
