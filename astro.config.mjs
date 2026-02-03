10c31f37d36e89d6b8ad0ac7a02bc95f41d79465
Tayou <git@tayou.org>
2026-02-03 22:00:27 +0100

    introduce tailwind and daisyUI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://mc-launcher.tayou.org/",
     output: "static",
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
